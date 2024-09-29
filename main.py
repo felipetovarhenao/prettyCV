3d3441217d271767a709828df2b2fdf91a6d6124
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2024-09-28 23:40:30 -0400

    Update paths to MBP

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from datetime import date
 import sys
 
 cv = CV()
-cv_path = '../personal-website/src/json/cv.json'
-works_path = '../personal-website/src/json/work-catalog.json'
+cv_path = '../felipetovarhenao.github.io/src/json/cv.json'
+works_path = '../felipetovarhenao.github.io/src/json/work-catalog.json'
 cv.load_data(cv_path=cv_path, works_path=works_path)
 cv.compile()
 file_id = date.today()
@@ -13,4 +13,5 @@ file_doc = file + '.docx'
 if len(sys.argv) > 1 and sys.argv[1] == '--local':
     cv.write(file_doc, open_file=False)
 else:
-    cv.write(f'/Users/felipe-tovar-henao/My Drive/FTH Drive/CV/CV_{file_id}.docx')
+    cv.write(
+        f'/Users/felipetovarhenao/Google Drive/My Drive/FTH Drive/CV/CV_{file_id}.docx')
